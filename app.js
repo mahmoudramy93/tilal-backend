a13d4e288ae36bc79f49b815791a6f79f8e4240f
mahmoudramy84 <mahmoudramy84@gmail.com>
2026-02-05 15:51:23 +0200

    Test auth services

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middlewares/error.middleware');
 const { successResponse } = require('./utils/response');
+const authRoutes = require('./modules/auth/auth.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Tilal API is running...');
